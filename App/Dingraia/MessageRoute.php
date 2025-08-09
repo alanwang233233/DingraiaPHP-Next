7e69d18ae29587205d1001252b7c29cbaacc7ee4
Error404 <wangzihao2010@outlook.com>
2025-08-09 21:53:04 +0800

    机器人消息路由初步实现

---
+++ App/Dingraia/MessageRoute.php
@@ -0,0 +1,43 @@
+<?php
+namespace App\Dingraia;
+class MessageRoute
+{
+    /**
+     * 路由列表
+     * 格式:
+     * [
+     *     '/command <param>' => [
+     *         'action' => ['controllerName','actionName'],
+     *         'middleware' => ['middlewareName']
+     *     ],
+     *     '^/user \d+$' => [
+     *         'action' => callable,
+     *         'middleware' => object
+     *     ],
+     *     '/command3' => [
+     *         'action' => 'controllerName@actionName',
+     *         'middleware' => object
+     *     ],
+     * ]
+     * @var array
+     */
+    private array $routes = [];
+
+    /**
+     * 新路由
+     * @param string|array $routes
+     * @param array|string|callable $action 方法
+     * @param array|string|object $middleware 中间件
+     * @return void
+     */
+    public function map(string|array $routes, array|string|callable $action, array|string|object $middleware): void
+    {
+        $routes = is_string($routes) ? [$routes] : $routes;
+        foreach ($routes as $r) {
+            $this->routes[$r] = [
+                'action' => $action,
+                'middleware' => $middleware
+            ];
+        }
+    }
+}
