b96a98203bf0afe2be0fc367b54664d08739d4bf
Error404 <wangzihao2010@outlook.com>
2025-08-15 19:43:22 +0800

    配置文件示例

---
+++ Config/Config.example.php
@@ -0,0 +1,30 @@
+<?php
+return [
+    'redis' => [
+        'host' => 'localhost',
+        'port' => 6379,
+        'password' => '123456',
+        'database' => 'test',
+        'timeout' => 0
+    ],
+    'database' => [
+        'driver' => 'mysql',
+        'mysql' => [
+            'host' => 'localhost',
+            'port' => 3306,
+            'username' => 'root',
+            'password' => '123456',
+            'database' => 'test'
+        ],
+        'sqlite' => [
+            'database' => 'test.db'
+        ],
+        'pgsql' => [
+            'host' => 'localhost',
+            'port' => 5432,
+            'username' => 'root',
+            'password' => '123456',
+            'database' => 'test'
+        ]
+    ]
+];
