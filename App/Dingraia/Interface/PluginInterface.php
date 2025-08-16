f1193548dac4449e1f032493a08d96842d6e9ce2
Error404 <wangzihao2010@outlook.com>
2025-08-16 18:46:02 +0800

    插件接口修改

--- App/Dingraia/Interface/PluginInterface.php
+++ App/Dingraia/Interface/PluginInterface.php
@@ -1,10 +1,10 @@
 <?php
-namespace App\Dingraia\Plugin;
+namespace App\Dingraia\Interface;
 interface PluginInterface {
-    public function getName(): string;
-    public function getDescription(): string;
+    public function getInfo(): array;
     public function activate(): bool;
     public function deactivate(): bool;
     public function install(): bool;
     public function uninstall(): bool;
+    public function main();
 }
