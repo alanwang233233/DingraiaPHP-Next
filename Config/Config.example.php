226b7e384562400052762e27734845eecafb29e4
Error404 <wangzihao2010@outlook.com>
2025-08-16 18:44:03 +0800

    配置文件示例修改

--- Config/Config.example.php
+++ Config/Config.example.php
@@ -1,30 +1,32 @@
 <?php
-return [
-    'redis' => [
-        'host' => 'localhost',
-        'port' => 6379,
-        'password' => '123456',
-        'database' => 'test',
-        'timeout' => 0
-    ],
-    'database' => [
-        'driver' => 'mysql',
-        'mysql' => [
+return function () {
+    return [
+        'redis' => [
             'host' => 'localhost',
-            'port' => 3306,
-            'username' => 'root',
+            'port' => 6379,
             'password' => '123456',
-            'database' => 'test'
+            'database' => 'test',
+            'timeout' => 0
         ],
-        'sqlite' => [
-            'database' => 'test.db'
-        ],
-        'pgsql' => [
-            'host' => 'localhost',
-            'port' => 5432,
-            'username' => 'root',
-            'password' => '123456',
-            'database' => 'test'
+        'database' => [
+            'driver' => 'mysql',
+            'mysql' => [
+                'host' => 'localhost',
+                'port' => 3306,
+                'username' => 'root',
+                'password' => '123456',
+                'database' => 'test'
+            ],
+            'sqlite' => [
+                'database' => 'test.db'
+            ],
+            'pgsql' => [
+                'host' => 'localhost',
+                'port' => 5432,
+                'username' => 'root',
+                'password' => '123456',
+                'database' => 'test'
+            ]
         ]
-    ]
-];
+    ];
+};
